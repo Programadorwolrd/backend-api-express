bcd470e9638e7daeebfab593210f0833f846d1b5
philype <philypejorge1@gmail.com>
2024-02-09 15:52:52 -0300

    commit Brincando com objetos

---
+++ src/config.js
@@ -0,0 +1,9 @@
+//esse arquivo foi criado na mão
+//esse arquivo vai servir para 
+
+export const PORT =3000
+export const HOST = 'http://localhost'
+export const LABRADOR ='Labrador '
+export const MULHER = ' MULHER'
+
+export default { ola: "flavio" ,PORT, HOST, LABRADOR,MULHER}
--- src/server.js
+++ src/server.js
@@ -1,1 +1,23 @@
-console.log("Ola imundo");
+import express from "express"
+import {PORT,HOST} from  './config.js'
+import DADOS from './config.js'
+
+
+// import nomeAleatorio from ''
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${HOST}${PORT}`)
+})
+
+app.post('./produto',(req,res) => {
+    res.send('hello world')
+})
+
+
+console.log(DADOS.ola);
+console.log(DADOS.MULHER);

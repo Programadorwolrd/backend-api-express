0b239758baf2c17b529c01e7cc12408b14f45d66
philype <lellis.sena@aluno.ifsp.edu.br>
2024-03-08 15:10:30 -0300

    Minha Api

--- src/controllers/produtos/create.js
+++
@@ -1,6 +0,0 @@
-const create = (req, res) => {
-    const product = req.body
-    res.json({message: 'Esta é a rota POST /product/', product})
-}
-
-export default create
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,45 +1,16 @@
-//essa pasta router e esse arquivo foi criado na mao e aqui vamos gerenciar as rotas 
-import listAll from '../controllers/user/listAll'
-import getById from '../controllers/user/getByid'
-import create from '../controllers/user/create'
-import  update from '../controllers/user/update'
-import remove from '../controllersuse'
 import express from 'express'
+import listAll from '../controllers/user/listAll.js'
+import getById from '../controllers/user/getById.js'
+import create from '../controllers/user/create.js'
+import update from '../controllers/user/update.js'
+import remove from '../controllers/user/remove.js'
 
-   //           função de roteador  express.Router()
 const router = express.Router()
 
 router.get('/', listAll)
 router.get('/:id', getById)
 router.post('/', create)
-router.put('/', update)
+router.put('/:id', update)
 router.delete('/:id', remove)
-
-
-
-
-
-router.get('/', (req, res)=>{
-    res.json({message:'essa é a rota user user'})
-}
-)
-
-
-
-router.get('/id', (req, res)=>{
-    res.json({message:'essa é a rota user id '+'ID=' +id})
-}
-)
-router.put('/', (req, res)=>{
-    res.json({message:'essa é a rota é put do user'})
-}
-)
-
-router.delete('/', (req, res)=>{
-    res.json({message:'essa é a rota é delete do user'})
-}
-)
-
-
 
 export default router
